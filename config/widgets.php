0f3eaf2c100de7f105554564b07e90bfb36a77e0
David Persson <davidpersson@gmx.de>
2014-04-10 16:03:16 +0200

    widgets

--- config/widgets.php
+++ config/widgets.php
@@ -14,12 +14,17 @@ use lithium\g11n\Message;
 use cms_core\extensions\cms\Widgets;
 use ecommerce_core\models\Carts;
 use ecommerce_core\models\Orders;
+use cms_core\models\Users;
+use cms_core\models\VirtualUsers;
 
 extract(Message::aliases());
 
 Widgets::register('ecommerce_core', 'carts', [
 	'type' => Widgets::TYPE_COUNT_MULTIPLE_BETA,
 	'group' => Widgets::GROUP_DASHBOARD,
+	'url' => [
+		'controller' => 'Carts', 'action' => 'index', 'library' => 'ecommerce_core'
+	],
 	'data' => function() use ($t) {
 		$open = Carts::find('count', ['conditions' => ['status' => 'open']]);
 		$expired = Carts::find('count', ['conditions' => ['status' => 'expired']]);
@@ -34,9 +39,36 @@ Widgets::register('ecommerce_core', 'carts', [
 	}
 ]);
 
+Widgets::register('ecommerce_core', 'total_customers', [
+	'type' => Widgets::TYPE_COUNT_MULTIPLE_BETA,
+	'group' => Widgets::GROUP_DASHBOARD,
+	'url' => [
+		'controller' => 'Users', 'action' => 'index', 'library' => 'cms_core'
+	],
+	'data' => function() use ($t) {
+		$total = Users::find('count', ['conditions' => [
+			'is_active' => true,
+			'role' => ['customer', 'merchant']
+		]]);
+		$total += VirtualUsers::find('count', ['conditions' => [
+			'is_active' => true,
+			'role' => ['customer', 'merchant']
+		]]);
+
+		return [
+			'data' => [
+				$t('Customers') => $total
+			]
+		];
+	}
+]);
+
 Widgets::register('ecommerce_core', 'total_orders_value', [
 	'type' => Widgets::TYPE_COUNT_SINGLE_ALPHA,
 	'group' => Widgets::GROUP_DASHBOARD,
+	'url' => [
+		'controller' => 'Orders', 'action' => 'index', 'library' => 'ecommerce_core'
+	],
 	'data' => function() use ($t) {
 		$orders = Orders::find('all');
 		$result = null;
@@ -50,15 +82,9 @@ Widgets::register('ecommerce_core', 'total_orders_value', [
 				$result = $value;
 			}
 		}
-
 		return [
 			'title' => $t('Total Orders Value (net)'),
-			'data' => $value
-		];
-
-		return [
-			$t('Open Carts') => $open,
-			$t('Expired Carts') => $expired
+			'value' => $value->getNet()
 		];
 	}
 ]);
