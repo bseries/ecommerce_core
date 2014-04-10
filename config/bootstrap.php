1aec170b2215a762b93a991e70baa60342c8e10b
David Persson <davidpersson@gmx.de>
2014-04-10 14:11:30 +0200

    Adding version.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -10,6 +10,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
+define('ECOMMERCE_CORE_VERSION', '1.0.0');
+
 use cms_core\extensions\cms\Features;
 use cms_core\extensions\cms\Settings;
 use cms_core\extensions\cms\Panes;
