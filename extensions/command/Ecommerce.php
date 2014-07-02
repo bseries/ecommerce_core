dbdfabf6a4fe4ca3c5b4239db562c37d8b2bcd83
David Persson <davidpersson@gmx.de>
2014-07-02 15:12:13 +0200

    Adding recover stock command.

---
+++ extensions/command/Ecommerce.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Boutique Core
+ *
+ * Copyright (c) 2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace ecommerce_core\extensions\command;
+
+use ecommerce_core\models\Shipments;
+use ecommerce_core\models\Products;
+use ecommerce_core\models\Orders;
+
+class Ecommerce extends \lithium\console\Command {
+
+	public function recoverStock() {
+		$products = Products::find('all');
+		$orders = Orders::find('all', [
+			'conditions' => [
+				'status' => ['checked-out', 'processed']
+			]
+		]);
+
+		foreach ($products as $product) {
+			$this->out("Product #{$product->number}, `{$product->title}`");
+			$this->out('Current real stock: ' . $product->stock('real'));
+			$this->out('Current virtual stock: ' . $product->stock('virtual'));
+			$stock = $this->in('Enter new real stock:', ['default' => $product->stock('real')]);
+
+			$this->out('Now checking how much we need to deduce from real...');
+			foreach ($orders as $order) {
+				$cartPositions = $order->cart()->positions();
+
+				$shipment = $order->shipment();
+				// TODO Check if shipment is in correct status-
+				if (!in_array($shipment->status, ['shipping'])) {
+					continue;
+				}
+
+				foreach ($cartPositions as $cartPosition) {
+					$cartProduct = $cartPosition->product();
+
+					if ($cartProduct->id == $product->id) {
+						$this->out('Found order #' . $order->number);
+						$stock -= $cartPosition->quantity;
+					}
+				}
+			}
+			$this->out("Decrementing real stock by {$count}.");
+			$this->out("Final real stock is {$stock}; saving...");
+			$result = $product->save([
+				'stock' => $stock
+			], [
+				'whitelist' => ['id', 'stock']
+			]);
+			$this->out($result ? 'OK' : 'FAILED!');
+		}
+	}
+}
+
+?>
