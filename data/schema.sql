76eca9d72ca4e423c45b8ae564b5c0c3cab70f1b
David Persson <davidpersson@gmx.de>
2014-09-03 14:54:14 +0200

    Adding schema.

--- data/schema.sql
+++ data/schema.sql
@@ -193,3 +193,12 @@ CREATE TABLE `ecommerce_shipments` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `ecommerce_product_attributes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ecommerce_product_id` int(11) unsigned NOT NULL,
+  `key` varchar(250) NOT NULL DEFAULT '',
+  `value` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_key` (`ecommerce_product_id`,`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
