403cce86582a9f9103b5389b85ef1f7d4236f3d3
David Persson <davidpersson@gmx.de>
2014-09-11 14:12:07 +0200

    Sync initial schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,30 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: localhost (MySQL 10.0.10-MariaDB-log)
-# Datenbank: rainmap
-# Erstellungsdauer: 2014-06-03 14:11:01 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Export von Tabelle ecommerce_cart_positions
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_cart_positions`;
-
+-- Create syntax for TABLE 'ecommerce_cart_positions'
 CREATE TABLE `ecommerce_cart_positions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ecommerce_cart_id` int(11) unsigned NOT NULL,
@@ -36,13 +10,7 @@ CREATE TABLE `ecommerce_cart_positions` (
   KEY `ecommerce_product_id` (`ecommerce_product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle ecommerce_carts
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_carts`;
-
+-- Create syntax for TABLE 'ecommerce_carts'
 CREATE TABLE `ecommerce_carts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned DEFAULT NULL,
@@ -56,13 +24,7 @@ CREATE TABLE `ecommerce_carts` (
   KEY `virtual_user_id` (`virtual_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle ecommerce_orders
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_orders`;
-
+-- Create syntax for TABLE 'ecommerce_orders'
 CREATE TABLE `ecommerce_orders` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(200) NOT NULL,
@@ -93,13 +55,17 @@ CREATE TABLE `ecommerce_orders` (
   KEY `billing_address_id` (`billing_address_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Create syntax for TABLE 'ecommerce_product_attributes'
+CREATE TABLE `ecommerce_product_attributes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ecommerce_product_id` int(11) unsigned NOT NULL,
+  `key` varchar(250) NOT NULL DEFAULT '',
+  `value` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_key` (`ecommerce_product_id`,`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-# Export von Tabelle ecommerce_product_groups
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_product_groups`;
-
+-- Create syntax for TABLE 'ecommerce_product_groups'
 CREATE TABLE `ecommerce_product_groups` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL DEFAULT '',
@@ -114,38 +80,30 @@ CREATE TABLE `ecommerce_product_groups` (
   KEY `cover_media_id` (`cover_media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle ecommerce_product_prices
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_product_prices`;
-
+-- Create syntax for TABLE 'ecommerce_product_prices'
 CREATE TABLE `ecommerce_product_prices` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ecommerce_product_id` int(11) unsigned NOT NULL,
   `group` varchar(20) NOT NULL,
-  `price_currency` char(3) NOT NULL DEFAULT 'EUR',
-  `price_type` char(5) NOT NULL DEFAULT 'net',
-  `price` int(10) unsigned NOT NULL,
+  `tax_type` varchar(20) NOT NULL DEFAULT '',
+  `tax_rate` int(5) unsigned NOT NULL DEFAULT '0',
+  `amount_currency` char(3) NOT NULL DEFAULT 'EUR',
+  `amount_type` char(5) NOT NULL DEFAULT 'net',
+  `amount` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ecommerce_product_id` (`ecommerce_product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='no fixed tax_rate';
 
-
-
-# Export von Tabelle ecommerce_products
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_products`;
-
+-- Create syntax for TABLE 'ecommerce_products'
 CREATE TABLE `ecommerce_products` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ecommerce_product_group_id` int(11) unsigned DEFAULT NULL,
   `number` varchar(100) NOT NULL DEFAULT '',
   `title` varchar(250) NOT NULL DEFAULT '',
   `description` text,
+  `tags` varchar(250) DEFAULT NULL,
   `cover_media_id` int(11) unsigned DEFAULT NULL,
+  `ecommerce_brand_id` int(11) unsigned DEFAULT NULL,
   `stock` int(10) NOT NULL DEFAULT '0',
   `is_published` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
@@ -157,19 +115,34 @@ CREATE TABLE `ecommerce_products` (
   KEY `cover_media_id` (`cover_media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Create syntax for TABLE 'ecommerce_shipment_positions'
+CREATE TABLE `ecommerce_shipment_positions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ecommerce_shipment_id` int(11) unsigned DEFAULT NULL COMMENT 'NULL until assigned to shipment',
+  `description` varchar(250) NOT NULL,
+  `quantity` decimal(10,2) unsigned NOT NULL DEFAULT '1.00',
+  `tax_type` varchar(20) NOT NULL DEFAULT '',
+  `tax_rate` int(5) unsigned NOT NULL,
+  `amount_currency` char(3) NOT NULL DEFAULT 'EUR',
+  `amount_type` char(5) NOT NULL DEFAULT 'net',
+  `amount` int(10) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `billing_invoice_id` (`ecommerce_shipment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-# Export von Tabelle ecommerce_shipments
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ecommerce_shipments`;
-
+-- Create syntax for TABLE 'ecommerce_shipments'
 CREATE TABLE `ecommerce_shipments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `virtual_user_id` int(11) unsigned DEFAULT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'created',
   `number` varchar(200) DEFAULT NULL,
   `tracking` varchar(200) DEFAULT NULL,
   `method` varchar(100) NOT NULL DEFAULT '',
+  `tax_rate` int(4) unsigned NOT NULL,
+  `tax_note` varchar(250) NOT NULL DEFAULT '',
   `address_name` varchar(250) DEFAULT '',
   `address_company` varchar(250) DEFAULT NULL,
   `address_street` varchar(250) DEFAULT NULL,
@@ -177,6 +150,8 @@ CREATE TABLE `ecommerce_shipments` (
   `address_zip` varchar(100) DEFAULT NULL,
   `address_country` char(2) DEFAULT 'DE',
   `address_phone` varchar(200) DEFAULT NULL,
+  `terms` text,
+  `note` text,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -184,25 +159,7 @@ CREATE TABLE `ecommerce_shipments` (
   KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `ecommerce_product_attributes` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ecommerce_product_id` int(11) unsigned NOT NULL,
-  `key` varchar(250) NOT NULL DEFAULT '',
-  `value` varchar(250) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_key` (`ecommerce_product_id`,`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+-- Augment other tables
 ALTER TABLE `users` ADD `has_accepted_terms` TINYINT(1)  UNSIGNED  NOT NULL  DEFAULT '0'  AFTER `is_notified`;
 ALTER TABLE `users` ADD `shipping_address_id` int(11) unsigned DEFAULT NULL AFTER `timezone`;
 
