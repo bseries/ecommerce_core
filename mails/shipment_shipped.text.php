e1f223210139f505e3d3458e49de64d74200876d
David Persson <davidpersson@gmx.de>
2014-11-06 12:27:06 +0100

    Positions in shipment mail.

--- mails/shipment_shipped.text.php
+++ mails/shipment_shipped.text.php
@@ -2,3 +2,10 @@ Hello <?= $user->name ?>,
 
 Your order #<?= $order->number ?> from <?= $this->date->format($item->created, 'date') ?> has
 just been shipped.
+
+Positions
+
+<?php foreach ($order->positions() as $position): ?>
+- <?= $position->description ?>
+
+<?php endforeach ?>
