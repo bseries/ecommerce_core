43d5455c2300794d9730cff89ce17c34e16277e6
David Persson <davidpersson@gmx.de>
2015-01-22 17:40:06 +0100

    This is becoming 1.3.

--- config/misc.php
+++ config/misc.php
@@ -10,7 +10,7 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
-define('ECOMMERCE_CORE_VERSION', '1.2.0');
+define('ECOMMERCE_CORE_VERSION', '1.3.0');
 
 use li3_access\security\Access;
 use base_tag\models\Tags;
