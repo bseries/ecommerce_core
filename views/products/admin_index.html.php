0c714d36ffde376aafeb1cf59f9a128a923c2df6
David Persson <davidpersson@gmx.de>
2015-01-27 12:27:12 +0100

    Use new index.

--- views/products/admin_index.html.php
+++ views/products/admin_index.html.php
@@ -8,34 +8,36 @@ $this->set([
 ]);
 
 ?>
-<article class="view-<?= $this->_config['controller'] . '-' . $this->_config['template'] ?> use-list">
+<article
+	class="use-index-table"
+	data-endpoint-sort="<?= $this->url([
+		'action' => 'index',
+		'page' => $paginator->getPages()->current,
+		'orderField' => '__ORDER_FIELD__',
+		'orderDirection' => '__ORDER_DIRECTION__'
+	]) ?>"
+>
 
 	<div class="top-actions">
-		<?= $this->html->link($t('new product'), ['action' => 'add', 'library' => 'ecommerce_core'], ['class' => 'button add']) ?>
+		<?= $this->html->link($t('new product'), ['action' => 'add'], ['class' => 'button add']) ?>
 	</div>
 
 	<?php if ($data->count()): ?>
 		<table>
 			<thead>
 				<tr>
-					<td data-sort="is-published" class="flag is-published list-sort"><?= $t('publ.?') ?>
+					<td data-sort="is-published" class="flag table-sort"><?= $t('publ.?') ?>
 					<td class="media">
-					<td data-sort="title" class="emphasize title list-sort"><?= $t('Title') ?>
-					<td data-sort="number" class="emphasize number list-sort"><?= $t('Number') ?>
-					<td data-sort="stock" class="stock list-sort"><?= $t('Stock') ?>
-					<td data-sort="created" class="date created list-sort desc"><?= $t('Created') ?>
+					<td data-sort="title" class="emphasize table-sort"><?= $t('Title') ?>
+					<td data-sort="number" class="emphasize number table-sort"><?= $t('Number') ?>
+					<td data-sort="stock" class="stock table-sort"><?= $t('Stock') ?>
+					<td data-sort="modified" class="date table-sort desc"><?= $t('Modified') ?>
 					<td class="actions">
-						<?= $this->form->field('search', [
-							'type' => 'search',
-							'label' => false,
-							'placeholder' => $t('Filter'),
-							'class' => 'list-search'
-						]) ?>
 			</thead>
 			<tbody class="list">
 				<?php foreach ($data as $item): ?>
 				<tr data-id="<?= $item->id ?>">
-					<td class="flag is-published"><?= ($item->is_published ? '✓' : '×') ?>
+					<td class="flag"><?= ($item->is_published ? '✓' : '×') ?>
 					<td class="media">
 						<?php if ($cover = $item->cover()): ?>
 							<?= $this->media->image($cover->version('fix3admin'), [
@@ -47,9 +49,9 @@ $this->set([
 					<td class="emphasize stock">
 						<span><?= $item->stock('virtual') ?></span>
 						<span class="minor"><?= $item->stock('real') ?></span>
-					<td class="date created">
-						<time datetime="<?= $this->date->format($item->created, 'w3c') ?>">
-							<?= $this->date->format($item->created, 'date') ?>
+					<td class="date">
+						<time datetime="<?= $this->date->format($item->modified, 'w3c') ?>">
+							<?= $this->date->format($item->modified, 'date') ?>
 						</time>
 					<td class="actions">
 						<?= $this->html->link($t('delete'), ['id' => $item->id, 'action' => 'delete', 'library' => 'ecommerce_core'], ['class' => 'delete button']) ?>
@@ -61,4 +63,6 @@ $this->set([
 	<?php else: ?>
 		<div class="none-available"><?= $t('No items available, yet.') ?></div>
 	<?php endif ?>
+
+	<?=$this->view()->render(['element' => 'paging'], compact('paginator'), ['library' => 'base_core']) ?>
 </article>
