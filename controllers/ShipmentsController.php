5153ffcb769bf0a442316145fa891bd6d3b75670
David Persson <davidpersson@gmx.de>
2016-07-19 16:52:21 +0200

    Use new users listing.

--- controllers/ShipmentsController.php
+++ controllers/ShipmentsController.php
@@ -20,7 +20,6 @@ namespace ecommerce_core\controllers;
 use billing_core\models\Currencies;
 use ecommerce_core\models\Shipments;
 use ecommerce_core\ecommerce\shipping\Methods as ShippingMethods;
-use base_core\models\Users;
 use lithium\g11n\Message;
 use li3_flash_message\extensions\storage\FlashMessage;
 
@@ -30,6 +29,7 @@ class ShipmentsController extends \base_core\controllers\BaseController {
 	use \base_core\controllers\AdminAddTrait;
 	use \base_core\controllers\AdminEditTrait;
 	use \base_core\controllers\DownloadTrait;
+	use \base_core\controllers\UsersTrait;
 
 	public function admin_export_pdf() {
 		extract(Message::aliases());
@@ -53,12 +53,15 @@ class ShipmentsController extends \base_core\controllers\BaseController {
 	}
 
 	protected function _selects($item = null) {
-		$statuses = Shipments::enum('status');
-		$methods = ShippingMethods::enum();
-		$currencies = Currencies::find('list');
-		$users = [null => '-'] + Users::find('list', ['order' => 'name']);
+		if ($item) {
+			$statuses = Shipments::enum('status');
+			$methods = ShippingMethods::enum();
+			$currencies = Currencies::find('list');
+			$users = $this->_users($item, ['field' => 'user_id', 'empty' => true]);
 
-		return compact('methods', 'statuses', 'currencies', 'users');
+			return compact('methods', 'statuses', 'currencies', 'users');
+		}
+		return [];
 	}
 }
 
