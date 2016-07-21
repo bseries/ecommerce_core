af754712e69203b7bb345d8e3b50ccd23fc35001
David Persson <davidpersson@gmx.de>
2016-07-21 13:26:56 +0200

    Conditionally add from id field.

--- views/orders/admin_form.html.php
+++ views/orders/admin_form.html.php
@@ -24,7 +24,9 @@ $this->set([
 ?>
 <article>
 	<?=$this->form->create($item) ?>
-		<?= $this->form->field('id', ['type' => 'hidden']) ?>
+		<?php if ($item->exists()): ?>
+			<?= $this->form->field('id', ['type' => 'hidden']) ?>
+		<?php endif ?>
 
 		<div class="grid-row">
 			<div class="grid-column-left">
