dbd0b0e2ca12e28a296ab93ddc8c2bc190852482
David Persson <nperson@gmx.de>
2017-10-13 10:11:02 +0200

    Fix price tax type logic.

--- models/ProductPrices.php
+++ models/ProductPrices.php
@@ -17,9 +17,10 @@
 
 namespace ecommerce_core\models;
 
-use Exception;
 use AD\Finance\Price;
+use Exception;
 use billing_core\billing\ClientGroups;
+use billing_core\billing\TaxTypes;
 use ecommerce_core\ecommerce\aquisition\Methods as AquisitionMethods;
 
 class ProductPrices extends \base_core\models\Base {
@@ -44,12 +45,12 @@ class ProductPrices extends \base_core\models\Base {
 		]
 	];
 
-	public function method($entity) {
-		return AquisitionMethods::registry($entity->method);
-	}
-
 	public function group($entity) {
 		return ClientGroups::registry($entity->group);
+	}
+
+	public function taxType($entity) {
+		return TaxTypes::registry($entity->name);
 	}
 
 	public function amount($entity) {
@@ -61,9 +62,8 @@ class ProductPrices extends \base_core\models\Base {
 		);
 	}
 
-	// @deprecated
-	public function taxType($entity) {
-		throw new Exception('Removed.');
+	public function method($entity) {
+		return AquisitionMethods::registry($entity->method);
 	}
 
 	// @deprecated
@@ -72,15 +72,14 @@ class ProductPrices extends \base_core\models\Base {
 	}
 }
 
-// On save derive tax type from group
+// When given derive missing field values from tax type.
 ProductPrices::applyFilter('save', function($self, $params, $chain) {
 	$entity = $params['entity'];
 	$data =& $params['data'];
 
-	if (isset($data['group'])) {
-		$group = ClientGroups::registry($data['group']);
-		$data['tax_type'] = $group->taxType()->name();
-		$data['amount_rate'] = $group->taxType()->rate();
+	if (isset($data['tax_type'])) {
+		$taxType = TaxTypes::registry($data['tax_type']);
+		$data['amount_rate'] = $taxType->rate();
 	}
 	return $chain->next($self, $params, $chain);
 });
